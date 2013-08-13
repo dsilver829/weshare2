9aa5713f2499d391da828253b670a48993f41fa8
David Silver <dsilver@candidatemetrics.com>
2013-08-12 20:21:43 -0700

    initiating rails

---
+++ weshare/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Weshare::Application.config.secret_token = '25b74e01e2b7eea5d0320f250b7ade0aee204db6a4c744c679dd6a6a536eacb403e4f44d1346814a78725bbec48db846bb2a5c4685ce1dcb20d25381bce59d95'
